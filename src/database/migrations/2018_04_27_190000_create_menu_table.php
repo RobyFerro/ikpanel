d31596b56e0b378e13ab880a34f20653f6586a9a
Roberto Ferro <roberto.ferro@ikdev.eu>
2018-08-26 16:24:56 +0200

    Modificata tipologie colonne

--- src/database/migrations/2018_04_27_190000_create_menu_table.php
+++ src/database/migrations/2018_04_27_190000_create_menu_table.php
@@ -14,14 +14,14 @@ class CreateMenuTable extends Migration
     public function up()
     {
         Schema::create('menu', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('id_token',64)->nullable();
-            $table->string('name',50);
-            $table->string('route', 255)->nullable();
-            $table->string('icon', 50)->nullable();
-            $table->integer('relation',64)->nullable();
-            $table->integer('order')->nullable();
-            $table->timestamps();
+	        $table->string('id',64)->primary();
+	        $table->string('id_token',64)->nullable();
+	        $table->string('name',50);
+	        $table->string('route', 255)->nullable();
+	        $table->string('icon', 50)->nullable();
+	        $table->string('relation',64)->nullable();
+	        $table->integer('order')->nullable();
+	        $table->timestamps();
         });
 
         Schema::table('menu', function (Blueprint $table){
