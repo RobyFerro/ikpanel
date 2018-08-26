69b705a845f369a6214041f916448488887994df
Roberto Ferro <roberto.ferro@ikdev.eu>
2018-08-26 22:51:04 +0200

    Inserite dipendenze npm

--- src/IkpanelServiceProvider.php
+++ src/IkpanelServiceProvider.php
@@ -17,7 +17,7 @@ class IkpanelServiceProvider extends ServiceProvider
         $this->publishes([
         	__DIR__.'/public' => public_path('ikpanel/'),
 	        __DIR__.'/resources/assets' => resource_path('assets/'),
-	        __DIR__.'/package.json' => base_path()
+	        __DIR__.'/package.json' => base_path('/')
         ], 'ikpanel');
         
         $this->loadRoutesFrom(__DIR__ .'/routes/web.php');
