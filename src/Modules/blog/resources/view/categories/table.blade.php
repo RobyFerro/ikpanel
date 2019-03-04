c6bb6aa7045a784ddba2f5aec90bbecee180aecc
Roberto Ferro <roberto.ferro@ikdev.eu>
2019-03-04 10:22:43 +0100

    Fix authorization in blog category view

--- src/Modules/blog/resources/view/categories/table.blade.php
+++ src/Modules/blog/resources/view/categories/table.blade.php
@@ -24,19 +24,26 @@
 			<td style="text-align: right">
 				@if(is_null($cat->deleted_at))
 					<div class="btn-group" role="group" aria-label="edit-category">
-						<a href="{{admin_url('/mod/blog/categories/edit/' . $cat->id)}}" class="btn btn-info btn-sm">
-							{{ __('ikpanel-blog::blog.categories.show.buttons.actionEdit') }}
-						</a>
-						<button type="button" class="btn btn-sm btn-danger action-delete" data-id="{{$cat->id}}">
-							{{ __('ikpanel-blog::blog.categories.show.buttons.actionDelete') }}
-						</button>
+						@can('blog-categories.update')
+							<a href="{{admin_url('/mod/blog/categories/edit/' . $cat->id)}}"
+							   class="btn btn-info btn-sm">
+								{{ __('ikpanel-blog::blog.categories.show.buttons.actionEdit') }}
+							</a>
+						@endcan
+						@can('blog-categories.delete')
+							<button type="button" class="btn btn-sm btn-danger action-delete" data-id="{{$cat->id}}">
+								{{ __('ikpanel-blog::blog.categories.show.buttons.actionDelete') }}
+							</button>
+						@endcan
 					</div>
 				@else
-					<button class="btn btn-success btn-sm action-restore" style="min-width: 110px;"
-					        data-id="{{ $cat->id }}">
-						<i class="fas fa-undo-alt fa-fw"></i>
-						{{ __('ikpanel-blog::blog.categories.show.buttons.actionRestore') }}
-					</button>
+					@can('blog-categories.restore')
+						<button class="btn btn-success btn-sm action-restore" style="min-width: 110px;"
+						        data-id="{{ $cat->id }}">
+							<i class="fas fa-undo-alt fa-fw"></i>
+							{{ __('ikpanel-blog::blog.categories.show.buttons.actionRestore') }}
+						</button>
+					@endcan
 				@endif
 			</td>
 		</tr>
